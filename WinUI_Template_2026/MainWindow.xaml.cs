1a8893f72a95a5a1a4986594bc0a31b5f930a73b
tabosann <tabosann@example.com>
2026-06-15 02:25:33 +0900

    [ADD]各Gridのロード完了時間を計測するように

--- WinUI_Template_2026/MainWindow.xaml.cs
+++ WinUI_Template_2026/MainWindow.xaml.cs
@@ -26,6 +26,10 @@ namespace WinUI_Template_2026
     public sealed partial class MainWindow : Window
     {
         Stopwatch PerfStopwatch { get; init; } = Stopwatch.StartNew();
+        Stopwatch PerfStopwatch10 { get; init; } = Stopwatch.StartNew();
+        Stopwatch PerfStopwatch11 { get; init; } = Stopwatch.StartNew();
+        Stopwatch PerfStopwatch20 { get; init; } = Stopwatch.StartNew();
+        Stopwatch PerfStopwatch21 { get; init; } = Stopwatch.StartNew();
 
         public MainWindow()
         {
@@ -38,7 +42,35 @@ namespace WinUI_Template_2026
         private void RootGrid_Loaded(object sender, RoutedEventArgs e)
         {
             PerfStopwatch.Stop();
-            m_titleBarText.Text = $"Startup Time {PerfStopwatch.ElapsedMilliseconds} ms";
+            m_titleBarText.Text = $"Startup (Ticks, Time) = ({PerfStopwatch.ElapsedTicks}, {PerfStopwatch.ElapsedMilliseconds} ms)";
+        }
+
+        private void Grid10_Loaded(object sender, RoutedEventArgs e)
+        {
+            PerfStopwatch10.Stop();
+            m_grid10.Text = $"Loaded Ticks {PerfStopwatch10.ElapsedTicks}\n";
+            m_grid10.Text += $"Loaded Time {PerfStopwatch10.ElapsedMilliseconds} ms";
+        }
+
+        private void Grid11_Loaded(object sender, RoutedEventArgs e)
+        {
+            PerfStopwatch11.Stop();
+            m_grid11.Text = $"Loaded Ticks {PerfStopwatch11.ElapsedTicks}\n";
+            m_grid11.Text += $"Loaded Time {PerfStopwatch11.ElapsedMilliseconds} ms";
+        }
+
+        private void Grid20_Loaded(object sender, RoutedEventArgs e)
+        {
+            PerfStopwatch20.Stop();
+            m_grid20.Text = $"Loaded Ticks {PerfStopwatch20.ElapsedTicks}\n";
+            m_grid20.Text += $"Loaded Time {PerfStopwatch20.ElapsedMilliseconds} ms";
+        }
+
+        private void Grid21_Loaded(object sender, RoutedEventArgs e)
+        {
+            PerfStopwatch21.Stop();
+            m_grid21.Text = $"Loaded Ticks {PerfStopwatch21.ElapsedTicks}\n";
+            m_grid21.Text += $"Loaded Time {PerfStopwatch21.ElapsedMilliseconds} ms";
         }
     }
 }
